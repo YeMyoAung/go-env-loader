7edb74390992239aa22da6a6f4891027910e3282
Ye Myo Aung <26484667+YeMyoAung@users.noreply.github.com>
2025-01-22 10:52:26 +0700

    feat: Add GoEnvLoader implementation and tests

---
+++ go_env_loader_test.go
@@ -0,0 +1,28 @@
+package go_env_loader
+
+import (
+    "log"
+    "strings"
+    "testing"
+)
+
+type Config struct {
+    Name  string `json:"name" validate:"required"`
+    Foods string `json:"foods" validate:"required"`
+}
+
+func (c *Config) FoodArray() []string {
+    return strings.Split(c.Foods, ",")
+}
+
+func TestGoEnvLoader(t *testing.T) {
+    config, err := NewGoEnvLoader[Config](nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+    if config == nil {
+        t.Fatal("Config was null")
+    }
+
+    log.Println(config.Value)
+}
